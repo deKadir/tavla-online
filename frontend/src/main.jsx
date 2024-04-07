47543d230c0c9680b55b5534ac2b1fa8f6f1e627
deKadir <develioglu306@gmail.com>
2024-04-07 06:50:06 +0300

    chore: init styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+document.querySelector("html").setAttribute("data-theme", "light");
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
