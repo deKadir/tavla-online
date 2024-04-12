0fee67d80a0ce8477b9fe0b79512b48742b9e2a7
deKadir <develioglu306@gmail.com>
2024-04-12 15:45:06 +0300

    chore: change base url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import axios from "axios";
-const baseURL = "http://localhost:3333/";
+const baseURL = "https://tavla-online-1.onrender.com/";
 const req = axios.create({ baseURL });
 const socket = io(baseURL);
 const api = {
